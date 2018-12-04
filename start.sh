e00c7a7e726c05630cd5c9a2434cae26926d352d
Shuhel Ahmed <shuhel@ymail.com>
2018-12-04 18:18:44 +0600

    Add localstack

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ tmuxp load -d assets
 tmuxp load -d sso
 tmuxp load -d analytics-api
 tmuxp load -d cmp
-tmuxp load -d abcm
+# tmuxp load -d abcm
 tmuxp load -d conf-mgmt
 tmuxp load -d api-gateway
-tmuxp load -d kinesalite
+tmuxp load -d localstack
 tmuxp load -d aqrp
 tmuxp load -d aqs
 
