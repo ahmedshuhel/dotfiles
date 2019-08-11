8d2d7fbfca5f831674f705c75b56d9a48c880198
Shuhel Ahmed <shuhel@ymail.com>
2019-08-11 07:22:03 +0600

    Format script

--- clean_port.sh
+++ clean_port.sh
@@ -2,19 +2,17 @@
 
 killport()
 {
-	pids=`lsof -ti:$@`
+  pids=`lsof -ti:$@`
 
-	if [ ! "$pids" ]; then
-		echo "No proccess on port $@ found to kill"
-	else
-		echo "Killing proccesses $pids"
-		kill -9 $pids
-	fi
+  if [ ! "$pids" ]; then
+    echo "No proccess on port $@ found to kill"
+  else
+    echo "Killing proccesses $pids"
+    kill -9 $pids
+  fi
 }
 
 for i in 80 443 3000 3001 3002 4000 4100 5000 5010 7005 7847 8080 8443 8081 7070 9200 9201 4502 4503 9230 9235 9229 27017 35729
 do
-    killport $i
+  killport $i
 done
-
-
