f1120a35b5b547366dd69baf7c05d2c38724c1ba
Shuhel Ahmed <shuhel@ymail.com>
2019-09-12 16:59:19 +0600

    Add instance manager

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ mongoctl start cmp
 tmuxp load -d antivirus
 tmuxp load -d assets
 tmuxp load -d sso
+tmuxp load -d instance-manager
 tmuxp load -d analytics-api
 tmuxp load -d cmp
 # tmuxp load -d abcm
