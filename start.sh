2694f8988b3eb43a4d09e56c1c86020408594b3a
Shuhel Ahmed <shuhel@ymail.com>
2020-02-16 16:00:36 +0600

    Add config for work_request and pitch_manager

--- start.sh
+++ start.sh
@@ -9,9 +9,11 @@ tmuxp load -d sso
 tmuxp load -d instance-manager
 tmuxp load -d analytics-api
 tmuxp load -d cmp
-# tmuxp load -d abcm
+tmuxp load -d abcm
 tmuxp load -d aqs
 tmuxp load -d conf-mgmt
 tmuxp load -d api-gateway
 tmuxp load -d aqrp
+tmuxp load -d pitchmanager
+tmuxp load -d workrequest
 
