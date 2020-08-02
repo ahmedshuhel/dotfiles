e7f5fd305192d3533a3e42fbc5bdaf6d42a5e59e
Shuhel Ahmed <ahmedshuhel@newscred.com>
2020-08-02 19:06:33 +0600

    Add .pyenv.sh

---
+++ .pyenv.zsh
@@ -0,0 +1,13 @@
+export CFLAGS="-I$(brew --prefix openssl)/include"
+export LDFLAGS="-L$(brew --prefix openssl)/lib"
+export LDFLAGS="${LDFLAGS} -L$(brew --prefix zlib)/lib"
+export CPPFLAGS="${CPPFLAGS} -I$(brew --prefix zlib)/include"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} $(brew --prefix zlib)/lib/pkgconfig"
+export CPPFLAGS="${CPPFLAGS} -I$(brew --prefix sqlite)/include"
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
