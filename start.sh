a667f5b22769bab5d81b77d7ff00c4764f685560
Shuhel Ahmed <shuhel@ymail.com>
2020-11-10 17:58:46 +0600

    Update start script

--- start.sh
+++ start.sh
@@ -1,19 +1,7 @@
 #!/bin/zsh
 
-mongoctl start cmp
-
-tmuxp load -d localstack
-tmuxp load -d antivirus
-tmuxp load -d assets
-tmuxp load -d sso
-tmuxp load -d instance-manager
-tmuxp load -d analytics-api
 tmuxp load -d cmp
-tmuxp load -d abcm
-tmuxp load -d aqs
 tmuxp load -d conf-mgmt
-tmuxp load -d api-gateway
 tmuxp load -d aqrp
-tmuxp load -d pitchmanager
-tmuxp load -d workrequest
+tmuxp load -d assets
 
