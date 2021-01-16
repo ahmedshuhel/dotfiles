b5297996130b377348a6c7270374e3f16694bf7a
Shuhel Ahmed <shuhel@ymail.com>
2021-01-16 12:54:05 +0600

    Add visual studio stettings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,14 @@
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Robbyrussell
+
+Set-Alias ll ls
+
+Set-Alias vim nvim
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+	Import-Module "$ChocolateyProfile"
+}
+
