a48f4a631a709278511bb20b5d50f804c3e8dec0
Shuhel Ahmed <shuhel@ymail.com>
2021-07-20 15:38:56 +0600

    Enable window picker

--- lua/tree-nvim.lua
+++ lua/tree-nvim.lua
@@ -26,7 +26,7 @@ M.config = function()
     g.nvim_tree_hijack_netrw = 1
     g.nvim_tree_update_cwd = 0
     g.nvim_tree_special_files = {}
-    g.nvim_tree_disable_window_picker = 1
+    g.nvim_tree_disable_window_picker = 0
 
     g.nvim_tree_show_icons = {
         git = 0,
