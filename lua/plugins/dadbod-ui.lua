521001629436273491f62855882ac1dcf446c9ba
Shuhel Ahmed <shuhel@ymail.com>
2021-08-27 23:13:46 +0600

    Add db ui config

--- lua/plugins/dadbod-ui.lua
+++ lua/plugins/dadbod-ui.lua
@@ -4,6 +4,12 @@ return {
     config = function()
         g.db_ui_use_nerd_fonts = 1
         g.db_ui_show_help = 0
+        g.db_ui_save_location = '~/Workspace/devlife/sql_queries'
+        g.db_ui_table_helpers = {
+          mysql = {
+            Count = 'select count(*) from `{table}`'
+          }
+        }
     end
 }
 
