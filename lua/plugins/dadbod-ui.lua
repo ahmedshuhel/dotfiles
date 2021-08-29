6d71e843192fc469070b6d5b3dca5fe48c56dcba
Shuhel Ahmed <shuhel@ymail.com>
2021-08-29 19:55:08 +0600

    Disable float notification

--- lua/plugins/dadbod-ui.lua
+++ lua/plugins/dadbod-ui.lua
@@ -2,6 +2,7 @@ local g = vim.g
 
 return {
     config = function()
+        g.db_ui_force_echo_notifications = 1
         g.db_ui_use_nerd_fonts = 1
         g.db_ui_show_help = 0
         g.db_ui_save_location = '~/Workspace/devlife/sql_queries'
@@ -12,4 +13,3 @@ return {
         }
     end
 }
-
