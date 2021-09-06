f2677e8c3bc5addb757bcce795c6bd8f60d8a83a
Shuhel Ahmed <shuhel@ymail.com>
2021-09-06 18:36:06 +0600

    Save queries in project scope

--- lua/plugins/dadbod-ui.lua
+++ lua/plugins/dadbod-ui.lua
@@ -1,11 +1,15 @@
 local g = vim.g
+local fn = vim.fn
+
 
 return {
     config = function()
+        local dir = fn.finddir('.git/..', fn.expand('%:p:h'))
+
         g.db_ui_force_echo_notifications = 1
         g.db_ui_use_nerd_fonts = 1
         g.db_ui_show_help = 0
-        g.db_ui_save_location = '~/Workspace/devlife/queries'
+        g.db_ui_save_location = dir .. '/.nvim/db'
         g.db_ui_table_helpers = {
           mysql = {
             Count = 'select count(*) from `{table}`'
