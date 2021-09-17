0f5251e106741189e009d42b6cb5bd095e8fecea
Shuhel Ahmed <shuhel@ymail.com>
2021-09-17 21:33:08 +0600

    Open all fugitive windows `rightbelow` except `G blame`

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -5,6 +5,8 @@ local function config()
         autocmd FileType gitcommit setlocal spell
       augroup END
     ]], false)
+
+    vim.cmd [[command! -bang -nargs=? -range=-1 -complete=customlist,fugitive#Complete G   exe fugitive#Command(<line1>, <count>, +"<range>", <bang>0, (len("<mods>") == 0 && <q-args> != "blame" ? "rightbelow" : "<mods>"), <q-args>)]]
 end
 
 return {
