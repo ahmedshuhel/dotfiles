d9f7a49b90e30b8a183d31d5be64940257df9bec
Shuhel Ahmed <shuhel@ymail.com>
2021-10-03 11:51:07 +0600

    Add formatters and linters

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -3,11 +3,21 @@ local nls = require("null-ls")
 local function config()
   nls.config({
     sources = {
-      nls.builtins.formatting.stylua
-    }
+      nls.builtins.formatting.stylua.with({
+        condition = function(utils)
+          return utils.root_has_file(".stylua.toml")
+        end,
+      }),
+      nls.builtins.formatting.prettier,
+      nls.builtins.formatting.black,
+      nls.builtins.formatting.sqlformat,
+      nls.builtins.formatting.terraform_fmt,
+      nls.builtins.diagnostics.codespell,
+      nls.builtins.diagnostics.eslint_d,
+    },
   })
 end
 
 return {
-  config = config
+  config = config,
 }
