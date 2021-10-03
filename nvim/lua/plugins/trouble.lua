fc186742b42f7a7710f73b852a871d09cc33d484
Shuhel Ahmed <shuhel@ymail.com>
2021-10-03 13:52:40 +0600

    Close with `gq` matching fugitive

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -11,7 +11,7 @@ local function config()
     action_keys = { -- key mappings for actions in the trouble list
         -- map to {} to remove a mapping, for example:
         -- close = {},
-        close = "q", -- close the list
+        close = "gq", -- close the list
         cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
         refresh = "r", -- manually refresh
         jump = {"<cr>", "<tab>", "o"}, -- jump to the diagnostic or open / close folds
