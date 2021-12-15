981c1f094f638a0e01e72830b9df4bfebc55f37f
Shuhel Ahmed <shuhel@ymail.com>
2021-12-15 17:09:03 +0000

    Change hint icon

--- nvim/lua/plugins/maq-lspconfig.lua
+++ nvim/lua/plugins/maq-lspconfig.lua
@@ -110,7 +110,7 @@ local function config()
   lspSymbol("Error", "")
   lspSymbol("Warn", "")
   lspSymbol("Info", "")
-  lspSymbol("Hint", "")
+  lspSymbol("Hint", "•")
 
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = false,
