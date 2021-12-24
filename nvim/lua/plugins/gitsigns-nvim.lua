67b3451d1a17242d18055a3f540fce489ba8e197
Shuhel Ahmed <shuhel@ymail.com>
2021-12-24 23:21:00 +0000

    Make change less distracting

--- nvim/lua/plugins/gitsigns-nvim.lua
+++ nvim/lua/plugins/gitsigns-nvim.lua
@@ -6,9 +6,9 @@ local function config()
 
   gs.setup({
     signs = {
-      add = { hl = "DiffAdd", text = "▌", numhl = "GitSignsAddNr" },
-      change = { hl = "DiffChange", text = "▌", numhl = "GitSignsChangeNr" },
-      delete = { hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr" },
+      add = { hl = "DiffAdd", text = "", numhl = "GitSignsAddNr" },
+      change = { hl = "DiffChange", text = "", numhl = "GitSignsChangeNr" },
+      delete = { hl = "DiffDelete", text = "~", numhl = "GitSignsDeleteNr" },
       topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
       changedelete = { hl = "DiffChange", text = "~", numhl = "GitSignsChangeNr" },
     },
